360e0521ae5b8592b06ec7d503313e2984270f4f
chrisvasey <chrismvasey@gmail.com>
2015-05-17 13:37:56 +0100

    Added times visited

--- server/fixtures.js
+++ server/fixtures.js
@@ -4,41 +4,41 @@ if (Users.find().count() === 0) {
     firstName: "Chris",
     lastName: "Vasey",
     lastVisit: "15/05/2015",
-    timesVisted: "13"
+    timesVisited: "13"
   });
 
   Users.insert({
     firstName: "Grace",
     lastName: "Booyd",
     lastVisit: "16/05/2015",
-    timesVisted: "6"
+    timesVisited: "6"
   });
 
   Users.insert({
     firstName: "Dan",
     lastName: "Vasey",
     lastVisit: "17/05/2015",
-    timesVisted: "34"
+    timesVisited: "34"
   });
 
   Users.insert({
     firstName: "Jade",
     lastName: "Tang",
     lastVisit: "17/05/2015",
-    timesVisted: "9"
+    timesVisited: "9"
   });
 
   Users.insert({
     firstName: "Peter",
     lastName: "Sedgewick",
     lastVisit: "15/05/2015",
-    timesVisted: "15"
+    timesVisited: "15"
   });
 
   Users.insert({
     firstName: "Lucie",
     lastName: "Moss",
     lastVisit: "20/04/2015",
-    timesVisted: "13"
+    timesVisited: "13"
   });
 }
